739cb2820be9a2d1a2f9ff5bc350b140202c25dd
github username <github email address>
2018-03-21 12:51:52 -0400

    index page more info ingredients

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,60 +1,60 @@
 class CommentsController < ApplicationController
-    
-    def new 
+
+    def new
         @recipe = Recipe.find_by(id: params[:recipe_id])
        @comments = @recipe.comments
-        @comment = Comment.new(recipe_id: @recipe.id) 
+        @comment = Comment.new(recipe_id: @recipe.id)
     end
-    
-    def create 
+
+    def create
         @recipe = Recipe.find_by(id: params[:recipe_id])
         @comment = @recipe.comments.new(comment_params)
          if @comment.user = current_user
         #binding.pry
-            if @comment.save 
-                redirect_to @recipe
-            else 
+            if @comment.save
+                render json: @recipe, status: 201
+            else
                 render :new
              end
         end
     end
-    
+
     def show
         @recipe = Recipe.find_by(id: params[:recipe_id])
         @comment = @recipe.comments.find_by(id: params[:id])
     end
-    
+
     def edit
         @comment = Comment.find_by(id: params[:id])
         @recipe = Recipe.find_by(id: params[:recipe_id])
     end
-    
+
     def update
         @comment = Comment.find_by(id: params[:id])
         @recipe = Recipe.find_by(id: params[:recipe_id])
            if @comment.update(comment_params)
             redirect_to recipe_comment_path(@recipe.id, @comment)
-        else 
+        else
             render :edit
         end
     end
-            
-    
-    def destroy 
+
+
+    def destroy
           @comment = Comment.find(params[:id])
         if @comment.user = current_user
             @comment.delete
             redirect_to recipes_path
-         else 
+         else
             redirect_to recipe_comment_path(@comment)
         end
     end
 
-    
-    
+
+
     private
     def comment_params
         params.require(:comment).permit(:text, :recipe_id, :user_id)
     end
-    
+
 end
--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -2,5 +2,6 @@ class RecipeSerializer < ActiveModel::Serializer
   attributes :title, :id
 
   has_many :comments
+  has_many :ingredients
   belongs_to :user
 end
